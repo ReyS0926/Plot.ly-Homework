df5c398a62e14ff3d7e06c4f0c6def3c1d4e37f5
ReyS0926 <rey.sosa26@gmail.comm>
2021-03-24 18:22:27 -0400

    HW Done

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,4 @@
-function buildMetadata(sample) {
+function createMetadata(sample) {
     console.log(sample);
     d3.json("samples.json").then((data) => {
         var metadata = data.metadata;
@@ -7,11 +7,98 @@ function buildMetadata(sample) {
         console.log(resultsarray);
         var result = resultsarray[0];
         console.log(result);
-        var panel = d3.select("#sample-metadata");
-        panel.html("");
+        var demographic = d3.select("#sample-metadata");
+        demographic.html("");
         Object.entries(result).forEach(([key, value]) => {
-            panel.append("h6").text(`${key}: ${value}`);
+            demographic.append("h5").text(`${key}: ${value}`);
         });
     });
 }
 
+
+function createCharts(sample) {
+   
+    d3.json("samples.json").then((data) => {
+        var samples = data.samples;
+        var resultsarray = samples.filter(sampleobject => sampleobject.id == sample);
+        var result = resultsarray[0]
+
+        var ids = result.otu_ids;
+        var labels = result.otu_labels;
+        var values = result.sample_values;
+
+
+        //Bar chart
+        var barData = [
+            {
+                y: ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse(),
+                x: values.slice(0, 10).reverse(),
+                type: "bar",
+                orientation: "h"
+                
+                
+            }
+        ];
+        
+        var barLayout = {
+            title: "Top 10 Bacteria Cultured",
+            margin: { t: 30, l:150}
+        };
+        
+        Plotly.newPlot("bar", barData, barLayout);
+        
+        //Bubble Chart
+        
+        var layout = {
+            margin: { t: 0},
+            xaxis: { title: "IDs"},
+            hovermode: "closest",
+        };
+
+        var data = [
+            {
+                x: ids,
+                y: values,
+                text: labels,
+                mode: "markers",
+                marker: {
+                    color: ids,
+                    size: values,
+                }
+            }
+        ];
+
+        Plotly.plot("bubble", data, layout);
+    });
+}
+
+function optionChanged(id) {
+    createCharts(id);
+    createMetadata(id);
+}
+
+
+function init() {
+
+    var dropdown = d3.select("#selDataset");
+    
+        
+        d3.json("samples.json").then((data)=> {
+            console.log(data)
+    
+            
+            data.names.forEach(function(name) {
+                dropdown
+                    .append("option")
+                    .text(name)
+                    .property("value");
+            });
+    
+            
+            buildgraph(data.names[0]);
+            getDemoInfo(data.names[0]);
+
+        });
+    }
+
+init();
